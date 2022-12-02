65a520f27d15db1683cfe70f141028118852b2a2
imhrushikesh <hrushikeshdurgude76497@gmail.com>
2022-12-02 17:50:16 +0530

    updated

---
+++ sensor/exception.py
@@ -0,0 +1,18 @@
+import sys,os
+
+def error_message_detail(error, error_detail: sys):
+    pass
+  
+
+
+
+class SensorException(Exception):
+
+     def __init__(self,error_message, error_detail:sys):
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail)
+
+     def __str__(self):
+        return self.error_message
+
+
