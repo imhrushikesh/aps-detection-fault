ff15ab051bf92ceee06e9ba7dad42780f43cee55
imhrushikesh <hrushikeshdurgude76497@gmail.com>
2022-12-15 15:11:04 +0530

    updated

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -9,9 +9,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-
 class DataIngestion:
-
+    
     def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig ):
         try:
             logging.info(f"{'>>'*20} Data Ingestion {'<<'*20}")
@@ -23,8 +22,8 @@ class DataIngestion:
         try:
             logging.info(f"Exporting collection data as pandas dataframe")
             #Exporting collection data as pandas dataframe
-            df:pd.DataFrame = utils.get_collection_as_dataframe(
-                database_name=self.data_ingestion_config.database_name,
+            df:pd.DataFrame  = utils.get_collection_as_dataframe(
+                database_name=self.data_ingestion_config.database_name, 
                 collection_name=self.data_ingestion_config.collection_name)
 
             logging.info("Save data in feature store")
@@ -34,19 +33,18 @@ class DataIngestion:
 
             #Save data in feature store
             logging.info("Create feature store folder if not available")
-
-            #Create feature store folder
+            #Create feature store folder if not available
             feature_store_dir = os.path.dirname(self.data_ingestion_config.feature_store_file_path)
             os.makedirs(feature_store_dir,exist_ok=True)
             logging.info("Save df to feature store folder")
-
             #Save df to feature store folder
             df.to_csv(path_or_buf=self.data_ingestion_config.feature_store_file_path,index=False,header=True)
+
 
             logging.info("split dataset into train and test set")
             #split dataset into train and test set
             train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size)
-
+            
             logging.info("create dataset directory folder if not available")
             #create dataset directory folder if not available
             dataset_dir = os.path.dirname(self.data_ingestion_config.train_file_path)
